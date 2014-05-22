03d096288021924e67d78a40d909c7d966799c5f
ChrisBup <christopherheuberger@gmail.com>
2014-05-22 09:28:59 -0400

    test

--- db/migrate/20140520191338_create_collections.rb
+++
@@ -1,9 +0,0 @@
-class CreateCollections < ActiveRecord::Migration
-  def change
-    create_table :collections do |t|
-      t.integer :artwork_id
-      t.integer :curator_id
-      t.string :name
-    end
-  end
-end
