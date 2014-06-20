3858f67f565ca688be9d0c1d7138668a76668675
ChrisBup <christopherheuberger@gmail.com>
2014-06-20 15:39:32 -0400

    add artwork to collection and display image

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -10,13 +10,20 @@ end
 def results
   @artwork = Artwork.new
   @artworks = Artwork.find_in_bk(params[:artwork][:keyword])
-  Rails.logger.info @artworks.inspect
 end
 
 # POST /create artwork#create - (DB call to create)
 def create
-  artwork = Artwork.create artwork_params
-  redirect_to artwork_path(artwork)
+  # create collection if there isn't one
+  if current_curator.collections.none?
+    current_curator.collections << Collection.create
+  end
+  # get curator's first collection
+  collection = current_curator.collections.first
+  # add artwork to collection
+   artwork = Artwork.create artwork_params
+   collection.artworks << artwork
+  redirect_to curator_collection_path(current_curator, collection)
 end
 
 # GET /show artwork#show - (DB call HTML to show)
